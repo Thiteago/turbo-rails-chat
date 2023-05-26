3e38dfbd5fdc1bd2f5a2cb09fd08485a452046a7
Thiago <ti+thiago@seguroviagem.srv.br>
2023-05-26 13:30:31 -0300

    create validation and relation on room and user models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
+  #validate if name length is more than 2
+  validates :name, length: { minimum: 2 }
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
+  after_create_commit { broadcast_append_to "rooms" }
+  has_many :messages
 end
